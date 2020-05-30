741b7b105371438c8cca49eea0a8d0dd2996b5e3
vdelacou <vincent.delacourt+github@outlook.com>
2020-05-30 18:22:45 +0800

    add copy locales files to create auth challenge

--- scripts/init_cognito.sh
+++ scripts/init_cognito.sh
@@ -55,7 +55,7 @@ mkdir ts
 cd ts
 ../../../../../init_function_typescript.sh
 npm install crypto-secure-random-digit i18n
-npm install --save-dev @types/i18n
+npm install --save-dev @types/i18n copyfiles
 curl -o index.ts https://raw.githubusercontent.com/serverless-ninja/generator_serverless-ninja/master/generators/amplify/backend/function/createAuthChallenge/ts/index.ts
 curl -o send_email.ts https://raw.githubusercontent.com/serverless-ninja/generator_serverless-ninja/master/generators/amplify/backend/function/createAuthChallenge/ts/send_email.ts
 curl -o send_sms.ts https://raw.githubusercontent.com/serverless-ninja/generator_serverless-ninja/master/generators/amplify/backend/function/createAuthChallenge/ts/send_sms.ts
@@ -66,6 +66,8 @@ cd locales
 curl -o en.json https://raw.githubusercontent.com/serverless-ninja/generator_serverless-ninja/master/generators/amplify/backend/function/createAuthChallenge/ts/locales/en.json
 curl -o fr.json https://raw.githubusercontent.com/serverless-ninja/generator_serverless-ninja/master/generators/amplify/backend/function/createAuthChallenge/ts/locales/fr.json
 cd ..
+echo "`npx jq '.scripts += {copyfiles: "copyfiles ./locales/* ../src"}' package.json`" > package.json
+echo "`npx jq '.scripts += {tsc: "npm run lint && tsc && npm run copyfiles"}' package.json`" > package.json
 npm run tsc
 cd ../../../../../
 
