60eb2649a637f46d2a4386a0e35136402efac180
vdelacou <vincent.delacourt+github@outlook.com>
2020-06-01 18:02:53 +0800

    first create the cognito before change the code

--- scripts/generate_serverless-ninja_backend.sh
+++ scripts/generate_serverless-ninja_backend.sh
@@ -9,16 +9,6 @@ npm init private
 # Init amplify
 ./init_amplify.sh
 
-# Setup VSCode
-mkdir .vscode
-curl -o .vscode/extensions.json https://raw.githubusercontent.com/serverless-ninja/generator_serverless-ninja/master/generators/.vscode/extensions.json
-curl -o .vscode/settings.json https://raw.githubusercontent.com/serverless-ninja/generator_serverless-ninja/master/generators/.vscode/settings.json
-
-# Add Prettier and Editorconfig configuration
-curl -o .prettierrc https://raw.githubusercontent.com/serverless-ninja/generator_serverless-ninja/master/generators/.prettierrc
-curl -o .prettierignore https://raw.githubusercontent.com/serverless-ninja/generator_serverless-ninja/master/generators/.prettierignore
-curl -o .editorconfig https://raw.githubusercontent.com/serverless-ninja/generator_serverless-ninja/master/generators/.editorconfig
-
 # Init Cognito
 ./init_cognito.sh
 
@@ -35,4 +25,18 @@ curl -o .editorconfig https://raw.githubusercontent.com/serverless-ninja/generat
 ./init_function.sh
 
 # Update Function
-./upate_function.sh
+./update_function.sh
+
+# Setup VSCode
+mkdir .vscode
+curl -o .vscode/extensions.json https://raw.githubusercontent.com/serverless-ninja/generator_serverless-ninja/master/generators/.vscode/extensions.json
+curl -o .vscode/settings.json https://raw.githubusercontent.com/serverless-ninja/generator_serverless-ninja/master/generators/.vscode/settings.json
+
+Add Prettier and Editorconfig configuration
+curl -o .prettierrc https://raw.githubusercontent.com/serverless-ninja/generator_serverless-ninja/master/generators/.prettierrc
+curl -o .prettierignore https://raw.githubusercontent.com/serverless-ninja/generator_serverless-ninja/master/generators/.prettierignore
+curl -o .editorconfig https://raw.githubusercontent.com/serverless-ninja/generator_serverless-ninja/master/generators/.editorconfig
+
+# Download the function code
+./download_code.sh
+
