cf115df455fbe1620818142016f6384447cf582c
vdelacou <vincent.delacourt+github@outlook.com>
2020-06-01 18:12:31 +0800

    make the graphql in 2 parts

--- scripts/init_api.sh
+++ scripts/init_api.sh
@@ -11,9 +11,9 @@
     sleep 1; echo "n"; printf "\n"; `# Do you want to edit the schema now? No` \
 ) | amplify add api
 
-
-curl -o "amplify/backend/api/${PROJECT_NAME}GraphQL/schema.graphql" https://raw.githubusercontent.com/serverless-ninja/generator_serverless-ninja/master/generators/amplify/backend/api/graphQL/schema.graphql
-npx replace-in-file /SERVERLESSNINJA_/g "${PROJECT_NAME}" "amplify/backend/api/${PROJECT_NAME}GraphQL/schema.graphql" --isRegex
+curl -o "amplify/backend/api/${PROJECT_NAME}GraphQL/schema/tenant.graphql" https://raw.githubusercontent.com/serverless-ninja/generator_serverless-ninja/master/generators/amplify/backend/api/graphQL/schema/tenant.graphql
+npx replace-in-file /SERVERLESSNINJA_/g "${PROJECT_NAME}" "amplify/backend/api/${PROJECT_NAME}GraphQL/schema/tenant.graphql" --isRegex
+curl -o "amplify/backend/api/${PROJECT_NAME}GraphQL/schema/pokemon.graphql" https://raw.githubusercontent.com/serverless-ninja/generator_serverless-ninja/master/generators/amplify/backend/api/graphQL/schema/pokemon.graphql
 
 ( \
     sleep 1; echo -n $'\e'\[B; printf "\n"; `# Choose the code generation language target: typescript` \
